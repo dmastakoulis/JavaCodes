4431ffa85e7094fc0f6f2143ffbf012c0bd42729
dmastakoulis <dmastakoulis@gmail.com>
2025-05-19 13:39:32 +0300

    Delete src/CoffeShop directory

--- src/CoffeShop/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package CoffeShop;
-
-import java.util.List;
-
-public interface OrderRepository {
-    void addOrder(Order order);
-    List<Order> getOrdersByDate(String date);
-    double getTotalAmountByCustomer(String customerName);
-}
